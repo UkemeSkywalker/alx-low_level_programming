ccde569bf1cfadb12ce433cce64b50dcd6b1bffd
UkemeSkywalker <skywalkershadow9@gmail.com>
2021-06-19 14:47:04 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int main (void) {
 
-  char myStr[54] ="\"Programming is like building a multilingual puzzule\"";
+  char myStr[54] ="\"Programming is like building a multilingual puzzule";
   
   puts(myStr);
   return 0;
