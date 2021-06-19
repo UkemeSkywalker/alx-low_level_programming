e9b594396192b60c953d944d2a084dd99b91082f
UkemeSkywalker <skywalkershadow9@gmail.com>
2021-06-19 14:43:15 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int main (void) {
 
-  char myStr[] = "Programming is like building a multilingual puzzule";
+  char myStr[] ="\"Programming is like building a multilingual puzzule\"";
   
   puts(myStr);
   return 0;
