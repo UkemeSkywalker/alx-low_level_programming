7f5a92bcc6b202a19baf9bd98b39fb9503f57c1e
UkemeSkywalker <skywalkershadow9@gmail.com>
2021-06-20 09:41:18 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int intType;
 long longType;
 double doubleType;
 float floatType;
-printf("Size of char: %zu byte(s)\n", sizeof(charType));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(doubleType));
