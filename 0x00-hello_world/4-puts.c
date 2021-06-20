91bdc25339d56d9321586306284a8d5d08a389ad
UkemeSkywalker <skywalkershadow9@gmail.com>
2021-06-20 07:40:09 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
-int main (void) {
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
-  puts("\"Programming is like building a multilingual puzzule");
-  return 0;
-  
+int main(void)
+{
+	char string[50];
+
+	strcpy(string, "\"Programming is like building a multilingual puzzle");
+
+	puts(string);
+
+	return (0);
 }
