019a3a9a566baf6ded0337a091917c28b9925c13
UkemeSkywalker <skywalkershadow9@gmail.com>
2021-06-21 13:24:26 -0700

    Alphabets with new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+char A;
+
+for (A = 'a'; A <= 'z'; A++)
+{
+putchar(A);
+putchar('\n');
+}
+return (0);
+}
