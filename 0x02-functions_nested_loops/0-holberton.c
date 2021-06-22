460c620272f86dd63529ceab71bfca585866b394
UkemeSkywalker <skywalkershadow9@gmail.com>
2021-06-22 05:49:00 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+
+
+int main(void)
+{
+  char c[9] = "Holberton";
+  int a = 0;
+
+  for (a = 0; a < 9; a++)
+    {
+      _putchar(c[a]);
+
+    }
+
+  _putchar('\n');
+  return (0);
+}
