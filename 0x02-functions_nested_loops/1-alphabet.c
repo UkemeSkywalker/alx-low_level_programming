1f825a18c23b718f4dd61ecbdf5d2367796025cd
UkemeSkywalker <skywalkershadow9@gmail.com>
2021-06-22 12:54:19 -0700

    alphabets prototypes

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - code used _putchar to print the alphabet
+ *
+ *
+ */
+
+void print_alphabet(void)
+{
+
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+
+}
+
+_putchar(i);
+
+}
