d081cc4a5c5d18f0c2a5a35d7238b417c3d11354
UkemeSkywalker <skywalkershadow9@gmail.com>
2021-06-22 08:43:28 -0700

    Putchar loops

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,18 +1,21 @@
 #include "holberton.h"
 
-
+/**
+ * main - Entry point
+ *
+ * Return: 0.
+ */
 
 int main(void)
 {
-  char c[9] = "Holberton";
-  int a = 0;
+char c[9] = "Holberton";
+int a = 0;
 
-  for (a = 0; a < 9; a++)
-    {
-      _putchar(c[a]);
+for (a = 0; a < 9; a++)
+{
+_putchar(c[a]);
 
-    }
-
-  _putchar('\n');
-  return (0);
+}
+_putchar('\n');
+return (0);
 }
