bfaa20ccccfae52294bfce8aa5d1b8b737049862
UkemeSkywalker <skywalkershadow9@gmail.com>
2021-06-23 13:08:34 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - check the code for Holberton School students.
+ * @i: digit to me tested.
+ * Return: last value of digit.
+ */
+
+int print_last_digit(int i)
+
+{
+	i = i % 10;
+
+	if (i < 0)
+	{
+		i = i * -1;
+	}
+
+	_putchar('0' + i);
+
+	return (i);
+
+}
+
+
+  
